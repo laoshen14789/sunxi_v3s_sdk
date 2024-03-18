a684de6064f9245cf7d16d8880c18ac0cfd7852e
shen <664376944@qq.com>
2024-03-18 23:25:11 +0800

    加入交叉编译工具链

---
+++ toolchain/lib/gcc/arm-buildroot-linux-gnueabi/10.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/home/shen/git_rep/buildroot-2021.11.x/output/host --sysconfdir=/home/shen/git_rep/buildroot-2021.11.x/output/host/etc --enable-static --target=arm-buildroot-linux-gnueabi --with-sysroot=/home/shen/git_rep/buildroot-2021.11.x/output/host/arm-buildroot-linux-gnueabi/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/home/shen/git_rep/buildroot-2021.11.x/output/host --with-mpc=/home/shen/git_rep/buildroot-2021.11.x/output/host --with-mpfr=/home/shen/git_rep/buildroot-2021.11.x/output/host --with-pkgversion='Buildroot 2021.11.3' --with-bugurl=http://bugs.buildroot.net/ --without-zstd --disable-libquadmath --disable-libquadmath-support --enable-tls --enable-threads --without-isl --without-cloog --with-abi=aapcs-linux --with-cpu=cortex-a7 --with-fpu=vfpv4 --with-float=softfp --with-mode=thumb --enable-languages=c,c++ --with-build-time-tools=/home/shen/git_rep/buildroot-2021.11.x/output/host/arm-buildroot-linux-gnueabi/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a7" }, { "float", "softfp" }, { "mode", "thumb" }, { "fpu", "vfpv4" }, { "tls", "gnu" } };
